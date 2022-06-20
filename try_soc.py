2234fb32c728dd535a6e6a86ae9ec63a73f4203b
baoting <bao-ting@foxmail.com>
2022-06-20 20:07:12 +0800

    intergrate utility

---
+++ try_soc.py
@@ -0,0 +1,11 @@
+# author:   Ting Bao @ Tsinghua University
+# email:    bao-ting@foxmail.com
+#
+# use the primtive cell of selected data, use openmx to get the band structure with/without soc
+# compare the band to decide whether to consider SOC in the dataset generattion
+
+used_data='selected_data/ICSD_False+maxele_3+maxatom_8+ehull_0.1/'
+topath='ifsoc_data/'
+
+if __name__=='__main__':
+    
