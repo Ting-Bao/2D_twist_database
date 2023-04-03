d1f4b44b0a17f2fd734358dbf6ca3495c8a94284
baoting <bao-ting@foxmail.com>
2023-04-03 22:01:22 +0800

    add connected band

--- reconnect.py
+++ reconnect.py
@@ -1,9 +1,11 @@
 # author:   Ting Bao @ Tsinghua University
 # email:    bao-ting@foxmail.com
 # date:     2022.07
-
+# this code is to reconect and calculate and do statics on band width under VBM
 import json
 import os
+from utility.connect_interpolate_func import connect_band
+from utility.refineplot_reconnected_band import *
 
 datapath = 'data/infer_calculation/'
 
@@ -14,9 +16,15 @@ def main():
     pathlist=[]
     for mat in matlist:
         twlist = os.listdir(os.path.join(datapath, mat))
+        twlist = [i for i in twlist if os.path.isdir(os.path.join(datapath ,i))]
         for tw in twlist:
-            path = os.path.join(datapath, mat, tw)
+            path = os.path.join(datapath, mat, tw,'band')
             pathlist.append(path)
+    
+    for path in pathlist:
+        connect_band(inputpath = path, outputpath = path)
+        
+    
 
 if __name__ == '__main__':
     main()
