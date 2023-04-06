d6b81e83aa86a395b17b6c4c792b4253dab98e20
baoting <bao-ting@foxmail.com>
2023-04-06 14:19:51 +0800

    add scatter band obj

--- reconnect.py
+++ reconnect.py
@@ -1,9 +1,10 @@
 # author:   Ting Bao @ Tsinghua University
 # email:    bao-ting@foxmail.com
 # date:     2022.07
-# this code is to reconect and calculate and do statics on band width under VBM
+# this code is to reconect and calculate and do statics on band width
 import json
 import os
+import shutil
 import pandas as pd
 from tqdm import tqdm
 from utility.connect_interpolate_func import connect_band
@@ -11,8 +12,7 @@ from utility.refineplot_reconnected_band import *
 
 datapath = 'data/infer_calculation/'
 
-
-def main():
+def get_path():
     matlist = os.listdir(datapath)
     matlist = [i for i in matlist if os.path.isdir(datapath+i)]
     pathlist = []
@@ -22,15 +22,27 @@ def main():
         for tw in twlist:
             path = os.path.join(datapath, mat, tw, 'band')
             pathlist.append(path)
+    return pathlist
 
+def main():
+    pathlist = get_path()
     data = pd.DataFrame(columns=['Material formula', 'C2DB id', 'Twisted index', '#(half) occupied flat bands',
                                  'occbandwidth', '#occupied flat bands', 'uoccbandwidth'])
     index = 0
     for path in tqdm(pathlist):#
         print(path)
         connect_band(inputpath=path, outputpath=path, spline_interpolate=False)
-        # write band_reconnected.json, the fermi energy need further refinement (implemented above)
-        band_obj = load_process_band(readpath=path, savepath=path, dft_path=path+'/../dft_band/')
+        if 'C2-a6735a4a3797/2-1' in path:
+            connect_band(inputpath=path, outputpath=path, tol=0.3, spline_interpolate=False)
+        if 'HfBi2-17f1085c54c9/2-1' in path:
+            connect_band(inputpath=path, outputpath=path, tol=0.3, spline_interpolate=False)
+        if 'HfBi2-ed9b447c7b9c/2-1' in path:
+            connect_band(inputpath=path, outputpath=path, tol=0.3, spline_interpolate=False)
+    
+        # use auto parameters or manual parameter for plot 
+        band_obj, shift = load_process_band(readpath=path, savepath=path, dft_path=path+'/../dft_band/')
+        with open(path+'/align_band_plot.json','w',encoding='utf-8') as fp:
+            json.dump({'plot_range':band_obj.band_data['advise_plot_range'],'shift_to_match_dft':shift}, fp, indent = 2)
 
         # count the flat band with condition:
         # at most 10 (half) occupied bands
@@ -40,7 +52,7 @@ def main():
         else:
             temp = band_obj.band_data["homo_band_index"]
         uocp_flat_index = [i for i in band_obj.band_data['flat_index'] if i > temp and i < temp+11]
-        ocp_flat_index = [i for i in band_obj.band_data['flat_index'] if i > temp-11 and i <= temp]
+        ocp_flat_index = [i for i in band_obj.band_data['flat_index'] if i > temp-10 and i <= temp]
         if len(uocp_flat_index)+len(ocp_flat_index) == 0:
             continue
         else:
@@ -76,6 +88,13 @@ def main():
 
     print('finished!')
 
+def collect_data(savepath='figure/band_compare/'):
+    pathlist = get_path()
+    #'data/infer_calculation/HfBi2-17f1085c54c9/2-1/band'
+    for path in pathlist:
+        temp = path.split('/')
+        shutil.copy(path+'/band_compare.png',savepath+temp[2]+'_'+temp[3]+'.png')
 
 if __name__ == '__main__':
     main()
+    collect_data()
--- utility/refineplot_reconnected_band.py
+++ utility/refineplot_reconnected_band.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-# Author: LiYang@cmt.tsinghua
+# Author: BaoTing@cmt.tsinghua for RcBand, plot_broken_y and compare band
+# Author: LiYang@cmt.tsinghua for BandData
 # Date: 2021.04.08
 # Descripution: This python code is designed for calculate the fermi energy
 #                and plot the band structure use the *.Band file.
@@ -458,6 +459,334 @@ class RcBandData():
     self.band_data['advise_plot_range']=[-boundary,boundary]
     return plot_args
 
+class ScBandData():
+  '''scatter band data class
+  '''
+  def __init__(self, data_type):
+    self.data_type = data_type
+    self.band_data = {}
+    return
+
+  def file_read(self, filename):
+    with open(filename, encoding='utf-8') as frp:
+      self.file_lines = frp.readlines()
+    return
+  
+  def __read_openmx_basic_info(self):
+    '''Read int the basic information of the band'''
+    # Read in the basic info.
+    line = self.file_lines[0].split()
+    band_num_each_spin = int(line[0])
+    spin_num = int(line[1]) + 1
+    band_num = band_num_each_spin * spin_num
+    fermi_energy = float(line[2]) * HARTREE
+    
+    # Record the info.
+    self.band_data["band_num_each_spin"] = band_num_each_spin
+    self.band_data["spin_num"] = spin_num
+    self.band_data["band_num"] = band_num
+    self.band_data["fermi_energy"] = fermi_energy
+    
+    # Variours check
+    if spin_num not in [1, 2]:
+      print("[error] Spin Number ERROR!")
+      sys.exit(1)
+    return
+  
+  def __read_openmx_rlv(self):
+    '''Read in the openmx reciprocal lattice vector (rlv)'''
+    # Reciprocal lattice vector :: rlv
+    # Each row of the 'rlv' matrix is a basis vector
+    line = self.file_lines[1].split()
+    rlv = np.zeros((3, 3))
+    rlv[0, 0] = float(line[0])
+    rlv[0, 1] = float(line[1])
+    rlv[0, 2] = float(line[2])
+    rlv[1, 0] = float(line[3])
+    rlv[1, 1] = float(line[4])
+    rlv[1, 2] = float(line[5])
+    rlv[2, 0] = float(line[6])
+    rlv[2, 1] = float(line[7])
+    rlv[2, 2] = float(line[8])
+    self.band_data["rlv"] = rlv
+
+  def __read_openmx_kpath(self):
+    '''Read the openmx band kpath info'''
+    # Read in the kpath number
+    line = self.file_lines[2]
+    kpath_num = int(line)
+    self.band_data["kpath_num"] = kpath_num
+    # Read in the kpath block
+    kpath_densities = [1 for _ in range(kpath_num)]
+    kpath_vectors = [[[], []] for _ in range(kpath_num)]
+    kpath_symbols = [[] for _ in range(kpath_num)]
+    line_i = 2
+    for kpath_i in range(kpath_num):
+      line_i += 1
+      line = self.file_lines[line_i].split()
+      kpath_densities[kpath_i] = int(line[0])
+      kpath_vectors[kpath_i][0] = [float(line[1]), float(line[2]), 
+                                   float(line[3])]
+      kpath_vectors[kpath_i][1] = [float(line[4]), float(line[5]), 
+                                   float(line[6])]
+      kpath_symbols[kpath_i] = [line[7], line[8]]
+    kpoints_num = sum(kpath_densities)
+    # Record the data
+    self.band_data["kpath_densities"] = kpath_densities
+    self.band_data["kpath_vectors"] = kpath_vectors
+    self.band_data["kpath_symbols"] = kpath_symbols
+    self.band_data["kpoints_num"] = kpoints_num
+    return
+
+  def __sort_energys(self, energys):
+    '''sort the energys'''
+    kpoints_num = len(energys[0])
+    band_num = len(energys)
+    sorted_energys = np.zeros((band_num, kpoints_num))
+    for kpoints_i in range(kpoints_num):
+        current_k_column = energys[:, kpoints_i]
+        sorted_current_k_column = np.sort(current_k_column)
+        sorted_energys[:, kpoints_i] = sorted_current_k_column
+    return sorted_energys
+
+  def __read_openmx_energys(self):
+    '''Read in the OPENMX energys'''
+    band_num_each_spin = self.band_data["band_num_each_spin"]
+    kpoints_num = self.band_data["kpoints_num"]
+    spin_num = self.band_data["spin_num"]
+    ### Prepare the data array
+    spin_up_energys = np.zeros((band_num_each_spin, kpoints_num))
+    if spin_num == 2:
+      spin_dn_energys = np.zeros((band_num_each_spin, kpoints_num))
+    else:
+      spin_dn_energys = []
+    kpoint_vectors = [[] for _ in range(kpoints_num)]
+    ### Read in the data
+    line_i = 2 + self.band_data["kpath_num"]
+    for kpoint_i in range(kpoints_num):
+      # The kpoints line
+      line_i += 1
+      line = self.file_lines[line_i].split()
+      kpoint_vectors[kpoint_i] = [float(line[1]), 
+                                  float(line[2]), 
+                                  float(line[3])]
+      # The (spin-up) energys line
+      line_i += 1
+      line = self.file_lines[line_i].split()
+      for band_i in range(band_num_each_spin):
+        spin_up_energys[band_i, kpoint_i] = float(line[band_i]) * HARTREE
+      # The spin-down energys line
+      if spin_num == 2:
+        line_i += 2
+        line = self.file_lines[line_i].split()
+        for band_i in range(band_num_each_spin):
+          spin_dn_energys[band_i, kpoint_i] = float(line[band_i]) * HARTREE
+    ### Post Process the Band energys
+    sorted_energys = spin_up_energys
+    if spin_num == 2:
+      sorted_energys = np.concatenate((spin_up_energys, spin_up_energys),
+                                      axis=0)
+
+    # lihe: Read band from each calc
+    assert spin_num == 1
+    print(spin_up_energys.shape)
+    for root, folders, files in os.walk('egval_k'):
+        if 'egval.dat' in files:
+            index_k = int(os.path.split(root)[-1]) - 1
+            spin_up_energys[:, index_k] = np.loadtxt(os.path.join(root, 'egval.dat'))
+
+    # Sort the band
+    sorted_energys = self.__sort_energys(sorted_energys)
+    ### Record the data
+    self.band_data["kpoint_vectors"] = kpoint_vectors
+    self.band_data["spin_up_energys"] = spin_up_energys
+    self.band_data["spin_dn_energys"] = spin_dn_energys
+    self.band_data["sorted_energys"] = sorted_energys
+    return
+
+  def __cal_k_distance(self, rlv, beg_kpt_frac, end_kpt_frac, distance_unit=1):
+    '''Calcualte the k-cooridnate in Cartesian indicator'''
+    beg_kpt_cart = np.array([0.0, 0.0, 0.0])
+    end_kpt_cart = np.array([0.0, 0.0, 0.0])
+    #
+    # k_cart = k_frac * rlv
+    #                               __              __
+    #                               | b_1x b_1y b_1z |
+    #        = (kf_1, kf_2, kf_3) * | b_2x b_2y b_2z |
+    #                               | b_3x b_3y b_3z |
+    #                               --              --
+    #        = (kc_x, kc_y, kc_z)
+    #
+    for xyz in range(3):
+      beg_kpt_cart[xyz] = 0.0
+      for b_i in range(3):
+        beg_kpt_cart[xyz] += beg_kpt_frac[b_i] * rlv[b_i, xyz]
+        end_kpt_cart[xyz] += end_kpt_frac[b_i] * rlv[b_i, xyz]
+    # Calculate the k distance of the two kpoints
+    k_distance = math.sqrt(sum((beg_kpt_cart-end_kpt_cart) ** 2))
+    k_distance /= distance_unit
+    return k_distance
+
+  def __get_kpt_coords(self, distance_unit):
+    '''Get the coords of each kpoints in k-space'''
+    kpath_num = self.band_data["kpath_num"]
+    kpath_vectors = self.band_data["kpath_vectors"]
+    kpoint_vectors = self.band_data["kpoint_vectors"]
+    kpath_densities = self.band_data["kpath_densities"]
+    rlv = self.band_data["rlv"]
+    kpoints_num = self.band_data["kpoints_num"]
+    ### Prepare the data list
+    hsk_distance_list = np.zeros(kpath_num)
+    sum_hsk_distance_list = np.zeros(kpath_num)
+    kpoints_coords = np.zeros(kpoints_num)
+    hsk_coords = np.zeros(kpath_num+1)
+    ### Get the distance for high-symmetry kpoints
+    for kpath_i in range(kpath_num):
+      start_hsk = kpath_vectors[kpath_i][0]
+      end_hsk = kpath_vectors[kpath_i][1]
+      hsk_distance_list[kpath_i] = \
+        self.__cal_k_distance(rlv, start_hsk, end_hsk, distance_unit)
+      sum_hsk_distance_list[kpath_i] = \
+        sum(hsk_distance_list[0:kpath_i+1])
+    hsk_coords[1:] = sum_hsk_distance_list
+    ### Get the distance in k-space of k-points on the k-path
+    kpoints_i = -1
+    for kpath_i in range(kpath_num):
+      # Count the Previous kpath distance
+      pre_path_distance = hsk_coords[kpath_i]
+      # Calculate the kpoints' distance in current kpath
+      for _ in range(kpath_densities[kpath_i]):
+        kpoints_i += 1
+        start_hsk = kpath_vectors[kpath_i][0]
+        end_hsk = kpoint_vectors[kpoints_i]
+        # The total distance equals to (pre_dis + curr_dis)
+        kpoints_coords[kpoints_i] = pre_path_distance + \
+          self.__cal_k_distance(rlv, start_hsk, end_hsk, distance_unit)
+    ### Record the data
+    self.band_data["hsk_coords"] = hsk_coords
+    self.band_data["kpoints_coords"] = kpoints_coords
+    return
+
+  def __refine_fermi_energy(self):
+    '''Refine the fermi energy and the center of HOMO and LUMO'''
+    fermi_energy = self.band_data["fermi_energy"]
+    energys = self.band_data["sorted_energys"]
+    band_num = self.band_data["band_num"]
+    kpoints_num = self.band_data["kpoints_num"]
+    spin_num = self.band_data["spin_num"]
+    # find the LUMO and HOMO
+    min_homo_ediff = fermi_energy - energys[0, 0]
+    homo_band_index = 0
+    homo_kpt_index = 0
+    min_lumo_ediff = energys[band_num-1, 0] - fermi_energy
+    lumo_band_index = band_num-1
+    lumo_kpt_index = 0
+    for band_i in range(band_num):
+      for kpoint_i in range(kpoints_num):
+        curr_energy = energys[band_i, kpoint_i]
+        lumo_ediff = curr_energy - fermi_energy 
+        homo_ediff = fermi_energy - curr_energy
+        if (lumo_ediff >= 0) and (lumo_ediff < min_lumo_ediff):
+          min_lumo_ediff = lumo_ediff
+          lumo_band_index = band_i
+          lumo_kpt_index = kpoint_i
+        elif (homo_ediff > 0) and (homo_ediff < min_homo_ediff):
+          min_homo_ediff = homo_ediff
+          homo_band_index = band_i
+          homo_kpt_index = kpoint_i
+    lumo_energy = energys[lumo_band_index, lumo_kpt_index]
+    homo_energy = energys[homo_band_index, homo_kpt_index]
+    refined_fermi_energy = (lumo_energy + homo_energy) / 2
+    
+    # lihe: no need to do refine
+    refined_fermi_energy = fermi_energy
+
+    # Shift the zero energy to the fermi level
+    self.band_data["origin_spin_up_energys"] = self.band_data["spin_up_energys"]
+    self.band_data["origin_spin_dn_energys"] = self.band_data["spin_dn_energys"]
+    self.band_data["origin_sorted_energys"] = self.band_data["sorted_energys"]
+    self.band_data["spin_up_energys"] -= refined_fermi_energy
+    self.band_data["sorted_energys"] -= refined_fermi_energy
+    if spin_num == 2:
+      self.band_data["spin_up_energys"] -= refined_fermi_energy
+    # Record the data
+    self.band_data["refined_fermi_energy"] = refined_fermi_energy
+    self.band_data["lumo_energy"] = lumo_energy
+    self.band_data["homo_energy"] = homo_energy
+    self.band_data["lumo_band_index"] = lumo_band_index
+    self.band_data["lumo_kpt_index"] = lumo_kpt_index
+    self.band_data["homo_band_index"] = homo_band_index
+    self.band_data["homo_kpt_index"] = homo_kpt_index
+    self.band_data["min_lumo_ediff"] = min_lumo_ediff
+    self.band_data["min_homo_ediff"] = min_homo_ediff
+    return
+
+  def __prepare_plot_kpt_symbol(self):
+    '''Prepare the kpoints symbols for the plot'''
+    kpath_num = self.band_data["kpath_num"]
+    kpath_symbols = self.band_data["kpath_symbols"]
+    # Prepare the symbol of k-axis (xtics)
+    hsk_symbols = ['' for _ in range(kpath_num+1)]
+    # Set 
+    hsk_symbols[0] = kpath_symbols[0][0]
+    for kpath_i in range(1, kpath_num):
+      if kpath_symbols[kpath_i][0] == kpath_symbols[kpath_i-1][1]:
+        hsk_symbols[kpath_i] = kpath_symbols[kpath_i][0]
+      else:
+        hsk_symbols[kpath_i] = "%s|%s" %(kpath_symbols[kpath_i - 1][1],
+                                         kpath_symbols[kpath_i][0])
+    hsk_symbols[-1] = kpath_symbols[-1][1]
+    ## Plot the Band
+    plot_hsk_symbols = []
+    for symbol in hsk_symbols:
+      symbol = symbol.replace("\\", "")
+      for greek_symbol in GREEK_SYMBOLS:
+        if greek_symbol == 'Gamma':
+          latex_greek_symbol = 'Γ'
+        else:
+          latex_greek_symbol = "$\\" + greek_symbol + "$"
+        symbol = re.sub(greek_symbol, "orz", symbol, 
+                        flags=re.I)
+        symbol = symbol.replace("orz", latex_greek_symbol)
+      symbol = re.sub(r'_\d+', lambda x:'$'+x[0]+'$', symbol)
+      plot_hsk_symbols.append(symbol)
+    ## Record the data
+    self.band_data["hsk_symbols"] = hsk_symbols
+    self.band_data["plot_hsk_symbols"] = plot_hsk_symbols
+    return
+ 
+  def get_band_data(self):
+    '''Get the band data'''
+    # Read in data
+    if self.data_type == 'openmx':
+      distance_unit = 2 * math.pi * BOHR
+      self.__read_openmx_basic_info()
+      self.__read_openmx_rlv()
+      self.__read_openmx_kpath()
+      self.__read_openmx_energys()
+    elif self.data_type == 'vasp':
+      print("[TODO]")
+      sys.exit(0)
+    else:
+      print("[TODO]")
+      sys.exit(0)
+    # Post process
+    self.__get_kpt_coords(distance_unit)
+    self.__refine_fermi_energy()
+    self.__prepare_plot_kpt_symbol()
+    return
+
+def band():
+  '''band functions'''
+  plot_args = get_command_line_input()
+  band_data_obj = BandData(plot_args["data_type"])
+  band_data_obj.file_read(plot_args["data_filename"])
+  band_data_obj.get_band_data()
+  band_save_to_json(band_data_obj.band_data, plot_args["file_tag"])
+  if not plot_args["no_plot"]:
+    band_plot(band_data_obj, plot_args)
+
+
 def get_command_line_input():
   '''Read in the command line parameters'''
   parser = argparse.ArgumentParser("Basic band plot parameters")
@@ -495,7 +824,6 @@ def get_command_line_input():
                "data_filename"   : args.data_filename,
                "no_plot"         : args.no_plot}
   return plot_args
-
 
 def band_save_to_json(data, file_tag):
   '''Save the band data to json file'''
@@ -772,9 +1100,9 @@ def band_plot_brokeny(band_data_obj, plot_args, cut_energy, highlight_num=1,save
   plt.savefig(savepath+'/band_reconnected_brokeny.svg', transparent=True)
   return 
 
-def compare_plot(band_data_obj,savepath='./',manual_align = True,highlight_band=[]):
+def compare_plot(band_data_obj,savepath='./',auto_align = True, manual_shift=None, highlight_band=[]):
   '''band plot function
-  manual align is to align the reconnected band to the DFT band
+  manual align is to align the reconnected band to the DFT band, which is prior to auto shift
   hightlight_band gives the band index of RC band to highlight(rg. flatband)
   '''
   hsk_coords = band_data_obj.band_data["hsk_coords"]
@@ -838,7 +1166,7 @@ def compare_plot(band_data_obj,savepath='./',manual_align = True,highlight_band=
   # for the fermi energy unmatch, here we have no choice but to mannually align it
   # we will search for the matched band near homo, to avoid band index error, 7 bands will be checked
   # this works only if the kmesh are the same
-  if manual_align:
+  if auto_align:
     diff_list=[]
     dft_homo = dft_data[band_data_obj.dft_data['homo_band_index']][:]
     for i in range(-3,4):
@@ -853,6 +1181,8 @@ def compare_plot(band_data_obj,savepath='./',manual_align = True,highlight_band=
     shift = shift/len(dft_homo)
   else:
     shift = 0
+  if abs(shift)>0.5:
+    shift = 0.0
   
   # Plot the Reconnected Band Structure
   for band_i in range(band_num_each_spin):
@@ -868,17 +1198,16 @@ def compare_plot(band_data_obj,savepath='./',manual_align = True,highlight_band=
   plt.savefig(savepath+'/band_compare.png', dpi=600, transparent=True)
   plt.savefig(savepath+'/band_compare.svg', transparent=True)
   plt.close()
-  return 
-
-
+  return shift
 
 def load_process_band(readpath,savepath,refine_fermi=True,get_bandwidth=True,\
-  advise_plot_range=True,plot=True,compare_dft=True,dft_path=None):
+  advise_plot_range=True,plot=True,compare_dft=True,dft_path=None,usescatter=None):
   '''
   by baot:
   load and process data from reconnected json
   add path argument for file input and output
   the options are not indepedent, see the source code
+  When usescatter is not none, the scatter path should be offered, eg. egval_k/
   '''
   plot_args = get_command_line_input()
   band_data_obj = RcBandData(plot_args["data_type"])
@@ -900,7 +1229,8 @@ def load_process_band(readpath,savepath,refine_fermi=True,get_bandwidth=True,\
   if not plot_args["no_plot"] and plot:
     band_plot(band_data_obj, plot_args,savepath=savepath)
     # band_plot_P4(band_data_obj, plot_args, cut_energy=[-0.09,0.09], highlight_num= 2 )
-
+  
+  shift = None
   if compare_dft:
     dft_data_obj = BandData(plot_args["data_type"])
     dft_data_obj.file_read(dft_path+'/openmx.Band')
@@ -912,9 +1242,9 @@ def load_process_band(readpath,savepath,refine_fermi=True,get_bandwidth=True,\
     else:
       temp = band_data_obj.band_data["homo_band_index"]
     highlight = [i for i in band_data_obj.band_data['flat_index'] if i> temp-11 and i<temp+1]
-    compare_plot(band_data_obj,savepath=savepath,highlight_band=highlight)
+    shift = compare_plot(band_data_obj,savepath=savepath,highlight_band=highlight)
 
-  return band_data_obj
+  return band_data_obj, shift
 #+----------------+
 #|  Main Process  |
 #+----------------+
